4585737d15e346803474d1a7eb4bd88d52d08a4e
Dung Huynh <dunghd.it@gmail.com>
2016-10-29 12:50:54 +0700

    + [Fix] Inline install chrome extension

--- MaomaoWeb/wwwroot/js/site.js
+++ MaomaoWeb/wwwroot/js/site.js
@@ -9,11 +9,13 @@ function onInstallSucess() {
 
 function onFail(err) {
   console.log(err);
+  // Error: Inline installs can only be initiated for Chrome Web Store items that have one or more verified sites.
+  window.location.href = 'https://chrome.google.com/webstore/detail/mm02ce/onkinoggpeamajngpakinabahkomjcmk';
 }
 
 function installExtension() {
   chrome.webstore.install(
-    'https://chrome.google.com/webstore/detail/mm02ce/onkinoggpeamajngpakinabahkomjcmk',
+    'https://chrome.google.com/webstore/detail/onkinoggpeamajngpakinabahkomjcmk',
     onInstallSucess,
     onFail);
 }
