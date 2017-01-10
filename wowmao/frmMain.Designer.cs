486a3c827d532d09a75c27c22b7fe130078d0d3b
dom <dom@lamoto.co.uk>
2017-01-10 16:00:44 +0800

    wowmao - golden cats, first

---
+++ wowmao/frmMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace wowmao
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.tvw = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tvw
+            // 
+            this.tvw.CheckBoxes = true;
+            this.tvw.Location = new System.Drawing.Point(12, 12);
+            this.tvw.Name = "tvw";
+            this.tvw.Size = new System.Drawing.Size(557, 462);
+            this.tvw.TabIndex = 0;
+            this.tvw.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvw_AfterSelect);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1312, 658);
+            this.Controls.Add(this.tvw);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "wowmao";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvw;
+    }
+}
+
---
+++ wowmao/frmMain.cs
@@ -0,0 +1,60 @@
+﻿using mmdb_model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mm_global;
+using mm_svc.Terms;
+using System.Diagnostics;
+
+namespace wowmao
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            InitTvw();
+        }
+
+        void InitTvw()
+        {
+            using (var db = mm02Entities.Create())
+            {
+                tvw.BeginUpdate();
+                var root = db.terms.Find(g.MAOMAO_ROOT_TERM_ID);
+                AddTerm(root, tvw.Nodes);
+                tvw.EndUpdate();
+            }
+        }
+
+        TreeNode AddTerm(term t, TreeNodeCollection add_to)
+        {
+            var tn = new TreeNode($"{t.name} (id={t.id}) #{t.occurs_count} corr={(t.corr??0).ToString("0.00")} XX={t.XX}");
+            add_to.Add(tn);
+            tn.Tag = t;
+            return tn;
+        }
+
+        private void tvw_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            var tn = e.Node;
+            if (tn.Nodes.Count == 0)
+            {
+                var parent_term = tn.Tag as term;
+                var correlated_terms = Correlations.Get(parent_term.name);
+                foreach (var term in correlated_terms)
+                {
+                    AddTerm(term, tn.Nodes);
+                }
+                tn.Expand();
+            }
+        }
+    }
+}
