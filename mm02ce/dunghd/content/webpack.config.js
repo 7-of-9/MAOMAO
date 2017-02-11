063a40718c1c32b2638d0c9240730ac9bb7bc3c1
Dung Huynh <dunghd.it@gmail.com>
2017-02-11 20:55:03 +0700

    + [Fix] Embed font

--- mm02ce/dunghd/content/webpack.config.js
+++ mm02ce/dunghd/content/webpack.config.js
@@ -21,16 +21,18 @@ module.exports = {
     devtool: 'source-map',
 
     module: {
-        loaders: [
-            {
+        loaders: [{
                 test: /\.scss$/,
                 loader: ExtractTextPlugin.extract(
                     'style', // The backup style loader
                     'css?sourceMap!sass?sourceMap',
                 ),
-                // loaders: ['style', 'css', 'sass'],
             },
-            { test: /\.(ttf|eot|otf)$/, loader: 'file' },
+            { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=[name].[ext]' },
+            { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=[name].[ext]' },
+            { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=[name].[ext]' },
+            { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=[name].[ext]' },
+            { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=[name].[ext]' },
             {
                 test: /\.css$/,
                 loaders: [
