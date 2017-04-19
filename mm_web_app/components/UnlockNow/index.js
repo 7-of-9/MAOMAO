b54784b71bf698b739fb643c7706a02f051e52c0
Dao Dang Cong <daoit151@gmail.com>
2017-04-19 20:03:17 +0700

    [WIP] Style landing page.

--- mm_web_app/components/UnlockNow/index.js
+++ mm_web_app/components/UnlockNow/index.js
@@ -16,7 +16,7 @@ const Button = styled.button`
 
 function UnlockNow ({ title, install }) {
   return (
-    <Button onClick={install}>{title} </Button>
+    <Button className="btn btn-unlock" onClick={install}>{title} </Button>
   )
 }
 
