85ba464c594757141440579f7b29af58a436a453
Dao Dang Cong <daoit151@gmail.com>
2017-04-22 13:10:33 +0700

    [FIX] Toggle button and text

--- mm02ce/dunghd/content/src/scripts/components/share/ShareOptions.jsx
+++ mm02ce/dunghd/content/src/scripts/components/share/ShareOptions.jsx
@@ -30,17 +30,18 @@ const ShareOptions = enhance(({ topics, active, onChange }) => {
   const isToggleTopic = !!topics.find(item => item.id === active);
   return (<div style={style} className="share-topic">
     <div className="switch-list">
-      <span className="share-topic-title"> Single URL: </span>
+      
       <div className="checkbox__content">
         <div className="switch-select">
           <Option key={guid()} value={active === 'site'} onToggle={() => { onChange('site'); }} />
+          <span className="share-topic-title"> Single URL: </span>
           <span className="type-name">just this page</span>
         </div>
       </div>
     </div>
     <div className="switch-list" style={{ display: topics.length > 0 ? '' : 'none' }}>
       <div className="checkbox__content">
-        <span className="share-topic-title"> Topics (Multiple URLs):</span>
+        
         <div className="checkbox__content">
           <div key={guid()} className="switch-select">
             <Option
@@ -50,6 +51,7 @@ const ShareOptions = enhance(({ topics, active, onChange }) => {
                 onChange((tld && tld.id) || (experimentalTopics[0] && experimentalTopics[0].id));
               }}
             />
+            <span className="share-topic-title"> Topics (Multiple URLs):</span>
           </div>
         </div>
       </div>
@@ -92,10 +94,10 @@ const ShareOptions = enhance(({ topics, active, onChange }) => {
         </div>
       </div>
     </div>
-    <div className="switch-list mb0">
-      <span className="share-topic-title"> All URLs: </span>
+    <div className="switch-list mb0">      
       <div className="switch-select">
         <Option key={guid()} value={active === 'all'} onToggle={() => { onChange('all'); }} />
+        <span className="share-topic-title"> All URLs: </span>
         <span className="type-name">My browsing history</span>
       </div>
     </div>
