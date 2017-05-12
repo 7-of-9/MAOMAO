dca5efd9505b7e4053b7f45421c2228e77f68bdb
Dung Huynh <dunghd.it@gmail.com>
2017-05-12 11:23:13 +0700

    [FIX] TOT on idle

--- mm02ce/app/js0/bs/bg_idle.js
+++ mm02ce/app/js0/bs/bg_idle.js
@@ -3,6 +3,18 @@ var idleState = 'active';
 chrome.idle.setDetectionInterval(BG_IDLE_DETECTION);
 
 chrome.idle.onStateChanged.addListener(function (state) {
-  log.info('idleState: ', idleState);
+  log.warn('idleState: ', state);
   idleState = state;
+  var session = session_get_by_url(window.sessionObservable.activeUrl);
+  if (session) {
+    if (idleState !== 'active') {
+      log.warn('stop tracking session', window.sessionObservable.activeUrl, session);
+      session_stop_TOT(session);
+    } else {
+      log.warn('start tracking session', window.sessionObservable.activeUrl, session);
+      session_start_TOT(session);
+    }
+  } else {
+    log.warn('not found session on url', window.sessionObservable.activeUrl);
+  }
 });
