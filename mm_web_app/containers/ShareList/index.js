b01426b24cf18612deb006903b1a2614d9b0f605
Dung Huynh <dunghd.it@gmail.com>
2017-05-28 01:51:38 +0700

    [WIP] Show all share list

--- mm_web_app/containers/ShareList/index.js
+++ mm_web_app/containers/ShareList/index.js
@@ -6,8 +6,9 @@
 
 import React from 'react'
 import { inject, observer } from 'mobx-react'
-// import { toJS } from 'mobx'
+import _ from 'lodash'
 import logger from '../../utils/logger'
+import { guid } from '../../utils/hash'
 
 const avatar = (user) => {
   if (user && (user.picture || user.avatar)) {
@@ -15,8 +16,6 @@ const avatar = (user) => {
   }
   return '/static/images/no-avatar.png'
 }
-
-// const MAX_COLORS = 12
 
 @inject('store')
 @inject('ui')
@@ -32,8 +31,8 @@ class ShareList extends React.Component {
 
   render () {
     const { user, userId } = this.props.store
-    const { entities: { myStreams, friendStreams, urls, topics }, result: { accept_shares } } = this.props.store.normalizedData
-    logger.warn('myStreams, friendStreams, urls, topics', myStreams, friendStreams, urls, topics)
+    const { entities: { friendStreams, urls, topics }, result: { accept_shares } } = this.props.store.normalizedData
+    logger.warn('friendStreams, urls, topics', friendStreams, urls, topics)
     return (
       <div className='share-modal-content'>
         <div className='modal-header'>
@@ -42,7 +41,7 @@ class ShareList extends React.Component {
         <div className='modal-body'>
           <div id='accordion' role='tablist' aria-multiselectable='true'>
             <div className='card card-topic'>
-              <div className='card-header' role='tab' id='headingOne' data-toggle='collapse' data-parent='#accordion' href='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
+              <div className='card-header' role='tab' id={`heading${userId}`} data-toggle='collapse' data-parent='#accordion' href={`#collapse${userId}`} aria-expanded='true' aria-controls={`collapse${userId}`}>
                 <div className='directional-user'>
                   <div className='share-image'>
                     <img className='share-object' src={avatar(user)} alt={userId} width='40' height='40' />
@@ -51,10 +50,10 @@ class ShareList extends React.Component {
                 </div>
               </div>
               {/* Your sharing */}
-              <div id='collapseOne' className='collapse show' role='tabpanel' aria-labelledby='headingOne'>
+              <div id={`collapse${userId}`} className='collapse show' role='tabpanel' aria-labelledby={`heading${userId}`}>
                 <div className='card-block'>
                   {accept_shares.map(receiver => (
-                    <ul className='timeline timeline-horizontal'>
+                    <ul key={guid()} className='timeline timeline-horizontal'>
                       <li className='timeline-item'>
                         <div className='timeline-badge'>
                           <img className='share-object' src={avatar(user)} alt={userId} width='40' height='40' />
@@ -103,54 +102,51 @@ class ShareList extends React.Component {
                 </div>
               </div>
             </div>
-            <div className='card card-topic'>
-              <div className='card-header' role='tab' id='headingOne' data-toggle='collapse' data-parent='#accordion' href='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
-                <div className='directional-user'>
-                  <div className='share-image'>
-                    <a href='#'><img className='share-object' src='https://scontent.xx.fbcdn.net/v/t1.0-1/s100x100/14702240_10207386391686714_2875182266540735639_n.jpg?oh=3fe0b8f61f0774ca75120127cd640154&oe=5957A4E8' alt='' width='40' height='40' /></a>
+            {_.map(friendStreams, friend => (
+              <div key={guid()} className='card card-topic'>
+                <div className='card-header' role='tab' id={`heading${friend.user_id}`} data-toggle='collapse' data-parent='#accordion' href={`#collapse${friend.user_id}`} aria-expanded='true' aria-controls={`collapse${friend.user_id}`}>
+                  <div className='directional-user'>
+                    <div className='share-image'>
+                      <img className='share-object' src={avatar(friend)} alt={friend.user_id} width='40' height='40' />
+                    </div>
+                    <div className='share-name'> {friend.fullname} </div>
                   </div>
-                  <div className='share-name'> Dominic Morris</div>
+                </div>
+                <div id={`#collapse${friend.user_id}`} className='collapse show' role='tabpanel' aria-labelledby={`heading${friend.user_id}`}>
+                  <div className='card-block'>
+                    {_.map(friend.list, item => (
+                      <ul className='timeline timeline-horizontal'>
+                        <li className='timeline-item share-line-right'>
+                          <div className='timeline-badge'>
+                            <img className='share-object' src={avatar(user)} alt={userId} width='51' height='51' />
+                          </div>
+                        </li>
+                        <li className='timeline-item'>
+                          <div className='timeline-badge'>
+                            <i className='fa fa-list' aria-hidden='true' />
+                          </div>
+                          <div className='timeline-panel'>
+                            <div className='tags-topic'>
+                              <span className='tags tags-color-1' rel='tag'>
+                                <span className='text-tag'>{item}</span>
+                              </span>
+                            </div>
+                          </div>
+                        </li>
+                        <li className='timeline-item'>
+                          <div className='timeline-badge'>
+                            <img className='share-object' src={avatar(friend)} alt={friend.user_id} width='51' height='51' />
+                          </div>
+                          <div className='timeline-panel'>
+                            <a href='#' className='btn btn-unfollow'>Unfollow</a>
+                          </div>
+                        </li>
+                      </ul>
+                    ))}
+                  </div>
                 </div>
               </div>
-              <div id='collapseTwo' className='collapse show' role='tabpanel' aria-labelledby='headingTwo'>
-                <div className='card-block'>
-                  <ul className='timeline timeline-horizontal'>
-                    <li className='timeline-item share-line-right'>
-                      <div className='timeline-badge'>
-                        <a href='#'>
-                          <img className='object-badge' src='https://scontent.xx.fbcdn.net/v/t1.0-1/s100x100/14702240_10207386391686714_2875182266540735639_n.jpg?oh=3fe0b8f61f0774ca75120127cd640154&oe=5957A4E8' alt='' width='51' height='51' />
-                        </a>
-                      </div>
-                      <div className='timeline-panel'>
-                        <a href='#' className='btn btn-related'>Unshare</a>
-                      </div>
-                    </li>
-                    <li className='timeline-item'>
-                      <div className='timeline-badge'>
-                        <i className='fa fa-list' aria-hidden='true' />
-                      </div>
-                      <div className='timeline-panel'>
-                        <div className='tags-topic'>
-                          <span className='tags tags-color-7' rel='tag'>
-                            <span className='text-tag'>University of California, Berkeley</span>
-                          </span>
-                        </div>
-                      </div>
-                    </li>
-                    <li className='timeline-item'>
-                      <div className='timeline-badge'>
-                        <a href='#'>
-                          <img className='object-badge' src='https://lh6.googleusercontent.com/-WLGCOsPN58Q/AAAAAAAAAAI/AAAAAAAAABc/pJzt8KW6Pxg/photo.jpg' alt='' width='51' height='51' />
-                        </a>
-                      </div>
-                      <div className='timeline-panel'>
-                        <a href='#' className='btn btn-unfollow'>Unfollow</a>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
