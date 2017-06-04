e6716f0f16c21cbaa970bcb2cdc9ccf4f3d255d3
daodc <daodcfrontend@gmail.com>
2017-06-04 23:49:47 +0700

    [FIXED] Make sticky footer with js.

--- mm_web_app/static/js/sticky.js
+++ mm_web_app/static/js/sticky.js
@@ -1,24 +1,27 @@
-$(function () {
-  function stickyFooter () {
-    var height = $(document).height()
+$(function() {
+  function stickyFooter() {
+    var height = $(window).innerHeight()
     $('.neal-page').css('min-height', height)
   }
 
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 71) {
-      $('.neal-page').addClass('sticky')
-      $('.standand-sort,.search-bar').addClass('animated slideInDown')
-    } else {
-      $('.neal-page').removeClass('sticky')
-      $('.standand-sort,.search-bar').removeClass('animated slideInDown')
-    }
-  })
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 71) {
+        $('.neal-page').addClass('sticky')
+        $('.standand-sort,.search-bar').addClass('animated slideInDown')
+      } else {
+        $('.neal-page').removeClass('sticky')
+        $('.standand-sort,.search-bar').removeClass('animated slideInDown')
+      }
+    })
     // sticky footer
   stickyFooter()
-  $(window).on('load', function (e) {
+  $(document).ready(function(e) {
     stickyFooter()
   })
-  $(window).on('resize', function (e) {
+  $(window).on('scroll', function() {
+      stickyFooter()
+  })
+  $(window).on('resize', function(e) {
     stickyFooter()
   })
 })
