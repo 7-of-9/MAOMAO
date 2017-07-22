75e4c3333b50f0af1dc6b0862944f0584afc2077
dom <dom@lamoto.co.uk>
2017-07-22 17:52:21 +0800

    [user] last_api_ip

--- mmdb_model/user.cs
+++ mmdb_model/user.cs
@@ -41,6 +41,7 @@ namespace mmdb_model
         public string last_api_cc { get; set; }
         public string last_api_city { get; set; }
         public string last_api_state { get; set; }
+        public string last_api_ip { get; set; }
     
         public virtual country country { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
