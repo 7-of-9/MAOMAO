d41ef61da92441e51ace2db7fe06a303ce209bef
Dom <dom@lamoto.co.uk>
2017-08-04 08:32:19 +0800

    +HtmlAgilityPack (svc)

---
+++ mm_svc/Discovery/GOOG.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mm_svc.Discovery {
+    public static class GOOG {
+
+        public static void Search(string term)
+        {
+
+        }
+    }
+}
