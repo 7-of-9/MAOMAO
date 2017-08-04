d9f5fb2a3bbe7b5c54b68d4e84f57d82a2e12717
Dom <dom@lamoto.co.uk>
2017-08-04 10:31:03 +0800

    google scraper v1

---
+++ mm_svc/tests/Tests/Discovery/Test_GoogDiscovery.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tests.Tests.Discovery
+{
+    [TestClass]
+    public class Test_GoogDiscovery
+    {
+        [TestMethod]
+        public void GoogDiscovery_Test0()
+        {
+            //var data = 
+                mm_svc.Discovery.GOOG.Search("chess singapore");
+        }
+    }
+}
